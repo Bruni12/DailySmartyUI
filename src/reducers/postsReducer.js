0fc76068c652883878a52850519f743989b074eb
Bruni12 <brunnetteboy@gmail.com>
2019-10-08 15:59:11 -0600

    added SET_RESULTS_POSTS reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,9 +1,10 @@
 import {
-    SET_RECENT_POSTS 
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types';
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
@@ -15,6 +16,12 @@ export default function(state = INIT_STATE, action) {
                     ...state, 
                     recentPosts
                    }
+         case SET_RESULTS_POSTS:
+                 const resultPosts = action.payload; 
+                 return {
+                     ...state,
+                     resultPosts
+                 }     
         default:
             return state;
     }
